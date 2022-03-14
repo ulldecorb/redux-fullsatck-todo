5e75bc9dddc9e652cfd3fef643512aa28887399b
ulldecorb <ausias.bertran@gmail.com>
2022-03-14 14:58:04 +0100

    Create sidebar component

---
+++ client/src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sidebar.css';
+
+export const Sidebar = function Sidebar() {
+  return (
+    <section className="sidebar">
+      <button type="button" className="sidebar__close-button">CLOSE</button>
+    </section>
+  );
+};
+
+export default Sidebar;
