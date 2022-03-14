25c926ac46a5d566010b54581f906fe0a904feb1
ulldecorb <ausias.bertran@gmail.com>
2022-03-14 15:17:41 +0100

    Add open functionality to sidebar Add close functionability to sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './sidebar.css';
 
 export const Sidebar = function Sidebar() {
+  const handleCloseSidebar = () => {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.right = '-13vw';
+  };
+
+  const handleOpenSidebar = () => {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.right = '0vw';
+  };
+
   return (
-    <section className="sidebar">
-      <button type="button" className="sidebar__close-button">CLOSE</button>
+    <section className="sidebar" id="sidebar">
+      <button type="button" onClick={handleOpenSidebar} className="sidebar__open-button">OPEN</button>
+      <button type="button" onClick={handleCloseSidebar} className="sidebar__close-button">CLOSE</button>
     </section>
   );
 };
